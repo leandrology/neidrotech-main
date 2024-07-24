10fd643728f8c819b91af427ba9a647e2f8c33b5
leandrology <leandrology@gmail.com>
2024-07-25 06:09:51 +0800

    Reverted commit, updated primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
         blacksection: "#141414",
         // primary: "#006BFF",
         // primaryho: "#0063EC",
-        primary: "#ff4255",
+        primary: "#f90a16",
         primaryho: "#b70c17",
         meta: "#20C5A8",
         waterloo: "#757693",
